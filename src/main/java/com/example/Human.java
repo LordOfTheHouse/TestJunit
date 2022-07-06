18319efbeccb35060c5039d4c575fb4c637fcac3
LordOfTheHouse <ke.s2013@yandex.ru>
2022-07-06 08:15:14 +0300

    Исправления

--- src/main/java/com/example/Human.java
+++ src/main/java/com/example/Human.java
@@ -1,27 +1,25 @@
 package com.example;
 
-
-public class Human 
-{
+public class Human {
     private int hp;
     private String name;
     private Reptiloyd creator;
 
-    public Human(String name, Reptiloyd creator){
+    public Human(String name, Reptiloyd creator) {
         this.name = name;
         this.creator = creator;
         hp = 100;
     }
 
-    public void dead(){
+    public void dead() {
         hp = 0;
     }
 
-    public boolean isDead(){
+    public boolean isDead() {
         return hp == 0;
     }
 
-    public boolean reptiloydIsDead(){
+    public boolean reptiloydIsDead() {
         return creator.isDead();
     }
 
@@ -31,6 +29,18 @@ public class Human
 
     public String getName() {
         return name;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCreator(Reptiloyd creator) {
+        this.creator = creator;
     }
 
 }
--- src/test/java/com/example/ReptiloydTest.java
+++ src/test/java/com/example/ReptiloydTest.java
@@ -1,72 +1,71 @@
 package com.example;
 
-import org.junit.Test;
-import java.lang.reflect.Method;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
 
-public class ReptiloydTest 
-{
-    
-    /*Задание 1*/
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+public class ReptiloydTest {
+
+    /* Просто тесты */
     @Test
-    public void createdHumanTest()
-    {
+    public void createdHumanTest() {
         Reptiloyd reptiloyd = new Reptiloyd();
         assertEquals("Human created", reptiloyd.createdHuman());
         assertEquals("The human has already been created", reptiloyd.createdHuman());
     }
 
     @Test
-    public void isDeadTest()
-    {
+    public void isDeadTest() {
         Reptiloyd reptiloyd = new Reptiloyd();
         assertFalse(reptiloyd.isDead());
         reptiloyd.dead();
         assertTrue(reptiloyd.isDead());
     }
 
+    /* Задание 1 */
     @Test
-    public void humanIsDeadTest()
-    {
+    public void getHumanNameTest() {
+        Human mockB = mock(Human.class);
+        doReturn("Georg").when(mockB).getName();
         Reptiloyd reptiloyd = new Reptiloyd();
-        reptiloyd.createdHuman();
-        assertFalse(reptiloyd.humanIsDead());
-        reptiloyd.humanDead();
-        assertTrue(reptiloyd.humanIsDead());
-
+        reptiloyd.setHuman(mockB);
+        assertEquals("Georg", reptiloyd.getHumanName());
     }
-    
-    /*Задание 2*/
+
+    /* Задание 2 */
     @Test
     public void spiedCreatedHumanTest() {
-        Reptiloyd testedClass = mock(Reptiloyd.class);
+        Reptiloyd testedClass = spy(Reptiloyd.class);
         testedClass.createdHuman();
         verify(testedClass, times(1)).createdHuman();
     }
-    
-    /*Задание 3 */
+
+    /* Задание 3 */
     @Test(expected = LimitedCloneException.class)
     public void testedMethodTest() throws LimitedCloneException {
         Reptiloyd testedClass = mock(Reptiloyd.class);
-        testedClass.createdHuman();  
+        testedClass.createdHuman();
         when(testedClass.cloneHuman()).thenThrow(new LimitedCloneException("Limited clone"));
         testedClass.cloneHuman();
     }
 
-    /*Задание 4 */
+    /* Задание 4 */
     @Test
     public void privateMethodTest() throws Exception {
         Reptiloyd reptiloyd = new Reptiloyd();
-        Method method = Reptiloyd.class.getDeclaredMethod("id");
+        Method method = Reptiloyd.class.getDeclaredMethod("getHp");
         method.setAccessible(true);
-        assertEquals(123, (int)method.invoke(reptiloyd));
+        assertEquals(10000, (int) method.invoke(reptiloyd));
     }
 
 }
